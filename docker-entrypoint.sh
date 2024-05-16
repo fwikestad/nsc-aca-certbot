7f8f0b3e090c27b4197973b8546d07328aaa55c3
= <=>
2024-05-16 08:37:07 +0200

    Remove dry-run

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,10 +3,10 @@
 # add crontab entry to renew the letsencrypt certificate
 # this cron job will run every day 11.00 P.M
 echo "Checking cert renew"
-certbot renew --dry-run
+certbot renew
 
 echo "adding crontab"
-echo "0 23 * * * certbot renew --dry-run" | crontab -
+echo "0 23 * * * certbot renew" | crontab -
 
 # start nginx
 echo "starting nginx"
